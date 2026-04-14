18a800bdf4dccb62c824dab5af2f89bf6c483a14
dannyb33 <dannyb3@vt.edu>
2026-04-14 23:01:30 +0200

    added text

--- script/fullscreen.js
+++ script/fullscreen.js
@@ -1,28 +1,39 @@
 const cards = document.querySelectorAll(".card");
 const overlay = document.getElementById("overlay");
 const overlayImg = document.getElementById("overlay-img");
+const overlayText = document.getElementById("overlay-text");
 
 cards.forEach(card => {
   card.addEventListener("click", () => {
 
     const img = card.querySelector("img");
-    const svg = card.querySelector("svg");
 
-    // 🖼 IMAGE CARD
-    if (img) {
-      overlay.innerHTML = ""; // clear previous SVG
-      overlay.appendChild(overlayImg);
-      overlayImg.style.display = "block";
-      overlayImg.src = img.src;
+    overlay.appendChild(overlayImg);
+    overlay.appendChild(overlayText);
+    overlayImg.style.display = "flex";
+    overlayImg.src = img.src;
+
+    console.log("test");
+    console.log(overlayText);
+
+    let out;
+
+    switch (card.id) {
+      case "chart2":
+        out = "Graphing the incidence of new COVID cases 3 years from the outset of the pandemic reveals some interesting patterns. Comprehensive reporting didn’t start for cases until mid-May, when France’s first lockdown was ending. From this point you can see cases start to rise in August, all the way until October when France shuts down again. Cases finally start to get under control with vaccination and distancing measures throughout 2021. In the beginning of 2022, we see a huge spike in cases with the Omicron variant, which was far more contagious than previous variants of the disease.";
+        break;
+      case "chart3":
+        out = "Deaths tend to follow a similar pattern to cases, but there’s a huge spike in deaths right at the pandemic’s onset. As the pandemic continued in full swing during late 2020 and early 2021, death rates remained high. The increase in preparation and knowledge of the disease likely helped France get death rates under control as time went on, however. Deaths do once again spike when Omicron spreads in early 2022.";
+        break;
+      case "chart4":
+        out = "After the COVID vaccine’s release in late 2020, French vaccination efforts begin in full in early 2021. This graph shows new incidences of full vaccination courses, when a person received the recommended dosage for each vaccine type. Even with the rise of the Omicron variant in early 2022, vaccination rates never reach the heights that they did earlier in the pandemic.";
+        break;
+      default:
+        out = "error";
+        break;
     }
 
-    // 📊 SVG CARD
-    else if (svg) {
-      overlay.innerHTML = ""; // clear image
-      const clone = svg.cloneNode(true);
-      overlay.appendChild(clone);
-      overlayImg.style.display = "none";
-    }
+    overlayText.innerHTML = '<p>'+out+'</p>';
 
     overlay.style.display = "flex";
   });
---
+++ script/map_text.js
@@ -0,0 +1,26 @@
+var options = document.getElementById("dataset-select");
+
+var text = document.getElementById("map-text");
+
+writeResult();
+
+options.addEventListener('change', writeResult);
+
+function writeResult() {
+    var selected = options.value;
+
+    console.log(options);
+
+    let out;
+
+    if (selected == "tx")
+    {
+        out = "Incidence refers to the number of people testing positive for covid relative to the total number of people per department. This measure generally correlates with the population density of France, with a few interesting notes. Paris actually isn’t that bad, with by far the highest population density with not the worst incidence rates. That award goes to the departments of Rhône and Bouches du Rhone, home to the cities of Lyon and Marseille respectively.";
+    } else if (selected == "to") {
+        out = "Occupancy rates refer to the proportion of COVID patients in intensive care units relative to the number of total beds in hospitals. Unsurprisingly, this correlates strongly to population density, with Île-de-France, home to Paris, having the highest occupancy rate.";
+    } else {
+        out = "Virus reproduction is the average number of people that an infected person can contaminate. The Course region, which is an island, has the worst rates for this measure. This may be due to the closed off nature of the landmass.";
+    }
+
+    text.innerHTML = '<p>'+out+'</p>'
+}
