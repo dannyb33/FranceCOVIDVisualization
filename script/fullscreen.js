32264052dc7b3c2dd09bf4e6b5e729da8ab9c026
dannyb33 <dannyb3@vt.edu>
2026-04-15 12:00:18 +0200

    references

--- script/fullscreen.js
+++ script/fullscreen.js
@@ -20,13 +20,13 @@ cards.forEach(card => {
 
     switch (card.id) {
       case "chart2":
-        out = "Graphing the incidence of new COVID cases 3 years from the outset of the pandemic reveals some interesting patterns. Comprehensive reporting didn’t start for cases until mid-May, when France’s first lockdown was ending. From this point you can see cases start to rise in August, all the way until October when France shuts down again. Cases finally start to get under control with vaccination and distancing measures throughout 2021. In the beginning of 2022, we see a huge spike in cases with the Omicron variant, which was far more contagious than previous variants of the disease.";
+        out = "Graphing the incidence of new COVID cases 3 years from the outset of the pandemic reveals some interesting patterns. Comprehensive reporting didn’t start for cases until mid-May, when France’s first lockdown was ending. From this point you can see cases start to rise in August, all the way until October when France shuts down again. Cases finally start to get under control with vaccination and distancing measures throughout 2021. In the beginning of 2022, we see a huge spike in cases with the Omicron variant, which was far more contagious than previous variants of the disease. [2]";
         break;
       case "chart3":
-        out = "Deaths tend to follow a similar pattern to cases, but there’s a huge spike in deaths right at the pandemic’s onset. As the pandemic continued in full swing during late 2020 and early 2021, death rates remained high. The increase in preparation and knowledge of the disease likely helped France get death rates under control as time went on, however. Deaths do once again spike when Omicron spreads in early 2022.";
+        out = "Deaths tend to follow a similar pattern to cases, but there’s a huge spike in deaths right at the pandemic’s onset. As the pandemic continued in full swing during late 2020 and early 2021, death rates remained high. The increase in preparation and knowledge of the disease likely helped France get death rates under control as time went on, however. Deaths do once again spike when Omicron spreads in early 2022. [2]";
         break;
       case "chart4":
-        out = "After the COVID vaccine’s release in late 2020, French vaccination efforts begin in full in early 2021. This graph shows new incidences of full vaccination courses, when a person received the recommended dosage for each vaccine type. Even with the rise of the Omicron variant in early 2022, vaccination rates never reach the heights that they did earlier in the pandemic.";
+      out = "After the COVID vaccine’s release in late 2020, French vaccination efforts begin in full in early 2021. This graph shows new incidences of full vaccination courses, when a person received the recommended dosage for each vaccine type. Even with the rise of the Omicron variant in early 2022, vaccination rates never reach the heights that they did earlier in the pandemic. [3]";
         break;
       default:
         out = "error";
--- script/map_text.js
+++ script/map_text.js
@@ -15,11 +15,11 @@ function writeResult() {
 
     if (selected == "tx")
     {
-        out = "Incidence refers to the number of people testing positive for COVID relative to the total number of people per department. This measure generally correlates with the population density of France, with a few interesting notes. Paris actually isn’t that bad, with by far the highest population density with not the worst incidence rates. That award goes to the departments of Rhône and Bouches du Rhone, home to the cities of Lyon and Marseille respectively.";
+        out = "Incidence refers to the number of people testing positive for COVID relative to the total number of people per department. This measure generally correlates with the population density of France, with a few interesting notes. Paris actually isn’t that bad, with by far the highest population density with not the worst incidence rates. That award goes to the departments of Rhône and Bouches du Rhone, home to the cities of Lyon and Marseille respectively. [1]";
     } else if (selected == "to") {
-        out = "Occupancy rates refer to the proportion of COVID patients in intensive care units relative to the number of total beds in hospitals. Unsurprisingly, this correlates strongly to population density, with Île-de-France, home to Paris, having the highest occupancy rate.";
+        out = "Occupancy rates refer to the proportion of COVID patients in intensive care units relative to the number of total beds in hospitals. Unsurprisingly, this correlates strongly to population density, with Île-de-France, home to Paris, having the highest occupancy rate. [1]";
     } else {
-        out = "Virus reproduction is the average number of people that an infected person can contaminate. The Course region, which is an island, has the worst rates for this measure. This may be due to the closed off nature of the landmass.";
+        out = "Virus reproduction is the average number of people that an infected person can contaminate. The Course region, which is an island, has the worst rates for this measure. This may be due to the closed off nature of the landmass. [1]";
     }
 
     text.innerHTML = '<p>'+out+'</p>'
